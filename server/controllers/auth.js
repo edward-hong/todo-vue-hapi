cc38963f257dbedc17d283c2f7b34eaa8ac6ea90
Edward Hong <edward.hong527@gmail.com>
2021-05-15 21:44:26 +1200

    send email on signup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,8 @@
 const Boom = require('@hapi/boom')
+const sgMail = require('@sendgrid/mail')
+const jwt = require('jsonwebtoken')
+
+sgMail.setApiKey(process.env.SENDGRID_KEY)
 
 exports.signup = db => async (request, h) => {
   const { name, email, password } = request.payload
@@ -12,12 +16,31 @@ exports.signup = db => async (request, h) => {
       return Boom.badRequest('Email is taken')
     }
 
-    const newUser = { _id: `auth:${email}`, name, password, email }
+    const token = jwt.sign(
+      { name, email, password },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      { expiresIn: '10m' }
+    )
 
-    await db.insert(newUser)
+    const emailData = {
+      from: process.env.EMAIL_FROM,
+      to: email,
+      subject: 'Account activation link',
+      html: `
+        <h1>Please use the following link to activate your account</h1>
+        <p>${process.env.CLIENT_URL}/activate/${token}</p>
+        <hr />
+        <p>This email may contain sensitive information</p>
+        <p>${process.env.CLIENT_URL}</p>
+      `,
+    }
 
-    return { message: 'Signup success! Please signin' }
+    await sgMail.send(emailData)
+
+    return {
+      message: `Email has been sent to ${email}. Follow the instructions to activate your account.`,
+    }
   } catch (err) {
-    console.log(err)
+    return Boom.badImplementation()
   }
 }
